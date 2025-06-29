8a950bdf61e04981e0efa5c91527a066824afdf0
Andrew <buikoan@gmail.com>
2025-06-29 18:23:11 +0300

    move task

--- src/TaskManager.Application/Tasks/Commands/CreateTaskCommand.cs
+++ src/TaskManager.Application/Tasks/Commands/CreateTaskCommand.cs
@@ -1,6 +1,5 @@
 ﻿using MediatR;
-using TaskManager.Application.Users.Commands;
-using Task = TaskManager.Domain.Tasks.Task;
+using TaskManager.Application.Tasks.Commands;
 
 namespace TaskManager.Application.Tasks;
 
@@ -9,25 +8,3 @@ public record CreateTaskCommand(
     string Description,
     Guid CreatorId, 
     IEnumerable<Guid> AssignedUserIds) : IRequest<TaskDto>;
-
-public class TaskDto
-{
-    public Guid Id { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public TaskStatus Status { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public UserDto Creator { get; set; }
-    public IEnumerable<UserDto> AssignedUsers { get; set; }
-
-    public TaskDto(Task task)
-    {
-        Id = task.Id;
-        Title = task.Title;
-        Description = task.Description;
-        Status = (TaskStatus)task.Status;
-        CreatedAt = task.CreatedAt;
-        Creator = new UserDto(task.Creator);
-        AssignedUsers = task.UserTasks.Select(ut => new UserDto(ut.User));
-    }
-}
---
+++ src/TaskManager.Application/Tasks/Commands/TaskDto.cs
@@ -0,0 +1,26 @@
+﻿using TaskManager.Application.Users.Commands;
+using Task = TaskManager.Domain.Tasks.Task;
+
+namespace TaskManager.Application.Tasks.Commands;
+
+public class TaskDto
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public TaskStatus Status { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public UserDto Creator { get; set; }
+    public IEnumerable<UserDto> AssignedUsers { get; set; }
+
+    public TaskDto(Task task)
+    {
+        Id = task.Id;
+        Title = task.Title;
+        Description = task.Description;
+        Status = (TaskStatus)task.Status;
+        CreatedAt = task.CreatedAt;
+        Creator = new UserDto(task.Creator);
+        AssignedUsers = task.UserTasks.Select(ut => new UserDto(ut.User));
+    }
+}
